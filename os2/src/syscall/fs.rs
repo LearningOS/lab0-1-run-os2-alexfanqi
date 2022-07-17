222022d2091dbb20fd43a8ccae5ebdfdcef6c965
Alex Fan <f1942357299@gmail.com>
2022-07-17 11:45:51 +0000

    Add memory access validation from sys_write

--- os2/src/syscall/fs.rs
+++ os2/src/syscall/fs.rs
@@ -1,6 +1,12 @@
+use crate::batch::{get_current_app, validate_app_addr_range_access};
+
 const FD_STDOUT: usize = 1;
 
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
+    let cur_app = get_current_app();
+    let buf_usize = buf as usize;
+    validate_app_addr_range_access(cur_app, buf_usize..buf_usize+len);
+    
     match fd {
         FD_STDOUT => {
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
